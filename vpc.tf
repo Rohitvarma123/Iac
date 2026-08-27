7f63d311c0792225cd6c93beebf0e13efa7316cd
Rohitvarma123 <128877028+Rohitvarma123@users.noreply.github.com>
2026-08-27 15:36:05 +0530

    creating vpc's in different regions

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+resource "aws_vpc" "satya_vpc"{
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "satya_vpc"
+    }
+}
+resource "aws_vpc" "dev_vpc"{
+    provider = aws.aws-west-1
+    cidr_block ="101.0.0.0/16"
+    tags ={
+        Name = "dev_vpc"
+    }
+}
